73bd317719208cbb2392e9b9862255c9d26e1e77
Vishnu <a.vishnu2097@gmail.com>
2020-07-18 23:00:57 +0530

    Fixed the Reset button

--- jsfile.js
+++ jsfile.js
@@ -3,30 +3,17 @@ const container = document.querySelector('#container');
 let div;
 for(let i=0; i < 256; i++) {
     div = document.createElement('div');
-    container.appendChild(div); //creates and appends div to #container div
-    div.setAttribute('class', 'grid') //adds class 'grid' to each div
-    div.addEventListener('mouseenter', () => {
+    container.appendChild(div);                         //creates and appends div to #container div
+    div.setAttribute('class', 'grid')                   //adds class 'grid' to each div
+    div.addEventListener('mouseenter', () => {          //colors the square div black  when hovered
         event.target.style.backgroundColor = 'black'
-    } //colors the square div black  when hovered
+    } 
     )   
 };
 
-function reset() {
-    for (i=1; i<=256; i++) {
+function reset() {                                      //resets grid by changing the background color of all the divs to white
+    for (i=1; i<=258; i++) {
         const x = document.getElementsByTagName('div')[i];
         x.style.backgroundColor = 'white';
     }
-}
-
-
-for(i=0, i<2, i++) {
-    btn = 
-}
-
-
-let btn; // = document.getElementsByTagName('button')[0];
-btn.classList.add('body');
-
-const body = document.getElementsByTagName('div')[0];
-body.classList.add('body')
-
+} 
